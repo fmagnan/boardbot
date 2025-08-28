29247b87829906dbe04a6b2d5c6e662d1037203e
Franck Magnan <fmagnan@gmail.com>
2025-08-28 16:57:07 +0200

    feat: ajout de nouvelles cartes

--- config/fantasy_realms.php
+++ config/fantasy_realms.php
@@ -76,5 +76,53 @@ return [
             'bonus' => [Glossary::ACTION_WITH_ANY_ONE_SUIT, [10, Glossary::SUIT_FLOOD]],
             //'penalty' => 'Blanks all Flames except Lightning',
         ],
+        'fire_elemental' => [
+            'name' => Glossary::CARD_FIRE_ELEMENTAL,
+            'suit' => Glossary::SUIT_FLAME,
+            'base_strength' => 4,
+            'bonus' => [Glossary::ACTION_FOR_EACH, [15, [Glossary::SUIT_FLAME]]],
+        ],
+        'air_elemental' => [
+            'name' => Glossary::CARD_AIR_ELEMENTAL,
+            'suit' => Glossary::SUIT_WEATHER,
+            'base_strength' => 4,
+            'bonus' => [Glossary::ACTION_FOR_EACH, [15, [Glossary::SUIT_WEATHER]]],
+        ],
+        'water_elemental' => [
+            'name' => Glossary::CARD_WATER_ELEMENTAL,
+            'suit' => Glossary::SUIT_FLOOD,
+            'base_strength' => 4,
+            'bonus' => [Glossary::ACTION_FOR_EACH, [15,[Glossary::SUIT_FLOOD]]],
+        ],
+        'earth_elemental' => [
+            'name' => Glossary::CARD_EARTH_ELEMENTAL,
+            'suit' => Glossary::SUIT_LAND,
+            'base_strength' => 4,
+            'bonus' => [Glossary::ACTION_FOR_EACH, [15, [Glossary::SUIT_LAND]]],
+        ],
+        'hydra' => [
+            'name' => Glossary::CARD_HYDRA,
+            'suit' => Glossary::SUIT_BEAST,
+            'base_strength' => 12,
+            'bonus' => [Glossary::ACTION_WITH_CARD, [28, Glossary::CARD_SWAMP]],
+        ],
+        'dwarvish_infantry' => [
+            'name' => Glossary::CARD_DWARVISH_INFANTRY,
+            'suit' => Glossary::SUIT_ARMY,
+            'base_strength' => 15,
+            'penalty' => [Glossary::ACTION_WITH_CARD, [2, Glossary::SUIT_ARMY]],
+        ],
+        'light_cavalry' => [
+            'name' => Glossary::CARD_LIGHT_CALVARY,
+            'suit' => Glossary::SUIT_ARMY,
+            'base_strength' => 17,
+            'penalty' => [Glossary::ACTION_WITH_CARD, [2, Glossary::SUIT_LAND]],
+        ],
+        'warlock_lord' => [
+            'name' => Glossary::CARD_WARLOCK_LORD,
+            'suit' => Glossary::SUIT_WIZARD,
+            'base_strength' => 25,
+            'penalty' => [Glossary::ACTION_FOR_EACH, [10, [Glossary::SUIT_LEADER, Glossary::SUIT_WIZARD]]],
+        ],
     ],
 ];
